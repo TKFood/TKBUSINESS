415ebc42f3de288fd579dcd80439b606ab847964
at3158 <tk290@tkfood.com.tw>
2021-04-21 11:09:37 +0800

    frmREPORTCOPMA

---
+++ TKBUSINESS/frmREPORTCOPMA.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using System.Text.RegularExpressions;
+using FastReport;
+using FastReport.Data;
+
+namespace TKBUSINESS
+{
+    public partial class frmREPORTCOPMA : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        int rownum = 0;
+        DataGridViewRow row;
+        int result;
+
+        public frmREPORTCOPMA()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void SETFASTREPORT()
+        {
+            StringBuilder SQL1 = new StringBuilder();
+
+            SQL1 = SETSQL();
+            Report report1 = new Report();
+            report1.Load(@"REPORT\久未交易.frx");
+
+            report1.Dictionary.Connections[0].ConnectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            TableDataSource table = report1.GetDataSource("Table") as TableDataSource;
+            table.SelectCommand = SQL1.ToString();
+
+            //report1.SetParameterValue("P1", textBox1.Text);
+            //report1.SetParameterValue("P2", textBox2.Text);
+            report1.Preview = previewControl1;
+            report1.Show();
+        }
+
+        public StringBuilder SETSQL()
+        {
+            StringBuilder SB = new StringBuilder();
+            //MessageBox.Show(strLineData);
+
+            SB.AppendFormat(@" 
+                            SELECT MV002 AS '業務',MA001 AS '客代',MA002 AS '客戶',TG003 AS '上次交易日'
+                            FROM (
+                            SELECT MV002,MA001,MA002
+                            ,(SELECT TOP 1 TG003 FROM [TK].dbo.COPTG WHERE TG004=MA001 ORDER BY TG003 DESC) AS 'TG003'
+                            FROM [TK].dbo.COPMA,[TK].dbo.CMSMV
+                            WHERE MA016=MV001
+                            AND COPMA.UDF01<>'Y'
+                            AND MV002 IN ('何姍怡','張釋予','林杏育','洪櫻芬','葉枋俐','蔡顏鴻')
+                            AND MA001 NOT LIKE '1%'
+                            ) AS TEMP
+                            WHERE DATEDIFF (DAY,CONVERT(datetime,TG003),GETDATE())>{0}
+                            ORDER BY MV002,MA001,MA002
+                            ", textBox1.Text.ToString());
+
+            return SB;
+
+        }
+        #endregion
+
+
+
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SETFASTREPORT();
+        }
+        #endregion
+    }
+}
