03e8ae5bfd028b4a56678226c684194adf983b7e
at3158 <tk290@tkfood.com.tw>
2022-10-26 09:34:30 +0800

    add fromCOPTATBSTOP

---
+++ TKBUSINESS/fromCOPTATBSTOP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using System.Text.RegularExpressions;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+
+namespace TKBUSINESS
+{
+    public partial class fromCOPTATBSTOP : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        int rownum = 0;
+        DataGridViewRow row;
+        int result;
+
+    
+
+        public fromCOPTATBSTOP()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
